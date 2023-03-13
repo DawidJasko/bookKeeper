85ad95d75c9687a6bdbb220cdcf011fd4b529003
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-03-13 23:44:07 +0100

    1 file modified

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModal() {
   modal.classList.add('show-modal');
@@ -41,6 +43,24 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Fetch Bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from local storege
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    // Create bookmarks array in localStorafe
+    bookmarks = [
+      {
+        name: 'Jacinto Desing',
+        url: 'https://jacinto.design',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  console.log(bookmarks);
+}
+
 // Handle Data from From
 function storeBookmark(e) {
   e.preventDefault();
@@ -49,10 +69,18 @@ function storeBookmark(e) {
   if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
     urlValue = `https://${urlValue}`;
   }
-  console.log(nameValue, urlValue);
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  console.log(JSON.stringify(bookmarks));
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 // Event Listener
